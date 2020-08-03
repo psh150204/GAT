8cee58464f4f4a45da3acc6c37dd74765ec4f222
psh150204 <psh131226@gmail.com>
2020-08-03 12:51:55 +0000

    complete GAT

--- model.py
+++ model.py
@@ -5,25 +5,31 @@ from layer import GraphConvolutionLayer, GraphAttentionLayer, SparseGraphConvolu
 
 # TODO step 1.
 class GCN(nn.Module):
-    def __init__(self, C, H, F, dropout):
+    def __init__(self, F, H, C, dropout):
         super(GCN, self).__init__()
-        self.layer1 = GraphConvolutionLayer(C, H)
-        self.layer2 = GraphConvolutionLayer(H, F)
+        self.layer1 = GraphConvolutionLayer(F, H)
+        self.layer2 = GraphConvolutionLayer(H, C)
         self.dropout = nn.Dropout(p = dropout)
 
     def forward(self, x, adj):
-        # X : a tensor with size [N, C]
-        x = self.dropout(F.relu(self.layer1(x, adj)))
-        return self.layer2(x, adj)
+        # X : a tensor with size [N, F]
+        
+        x = self.dropout(F.relu(self.layer1(x, adj))) # [N, H]
+        return self.layer2(x, adj) # [N, C]
     
 # TODO step 2.
 class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+    def __init__(self, F, H, C, dropout, alpha, K):
         super(GAT, self).__init__()
-        pass
+        self.layer1 = GraphAttentionLayer(F, H, K, alpha)
+        self.layer2 = GraphAttentionLayer(K * H, C, 1, alpha, concat = False)
+        self.dropout = nn.Dropout(p = dropout)
 
     def forward(self, x, adj):
-        pass
+        # x : a tensor with size [N, F]
+
+        x = self.dropout(F.relu(self.layer1(x, adj))) # [N, KH]
+        return self.layer2(x, adj) # [N, C]
 
 # TODO step 3.
 class SpGCN(nn.Module):
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import time
 import random 
 import argparse
 import glob
+import matplotlib.pyplot as plt
 
 import numpy as np 
 import torch
@@ -26,36 +27,44 @@ def main(args):
     device = torch.device('cuda' if(torch.cuda.is_available()) else 'cpu')
 
     # load the data
-    adj, features, labels, idx_train, idx_val, idx_test = load_data()
+    adj, features, labels, idx_train, idx_val, idx_test = load_data(args.dataset)
     features = features.to(device)
     labels = labels.to(device)
 
     # parameter intialization
-    N = features.size(0) # num_nodes
-    C = features.size(1) # num_of_features
+    N = features.size(0) # num_of_nodes
+    F = features.size(1) # num_of_features
     H = args.hidden # hidden nodes
-    F = labels.max().item() + 1 # num_classes
+    C = labels.max().item() + 1 # num_classes
     
     # for validation
     epochs_since_improvement = 0
     best_loss = 10.
 
     # init training object
-    network = GCN(C, H, F, args.dropout).to(device)
+    if args.model == 'GCN':
+        network = GCN(F, H, C, args.dropout).to(device)
+
+        # pre-processing
+        A_tilde = adj + torch.eye(N)
+        D_tilde_inv_sqrt = torch.diag(torch.sqrt(torch.sum(A_tilde, dim = 1)) ** -1)
+        adj = torch.mm(D_tilde_inv_sqrt, torch.mm(A_tilde, D_tilde_inv_sqrt)).to(device) # A_hat
+        
+    else:
+        network = GAT(F, H, C, args.dropout, args.alpha, args.n_heads).to(device)
+        adj = adj.to(device)
+    
     optimizer = optim.Adam(network.parameters(), lr = args.lr, weight_decay = args.weight_decay)
     criterion = nn.CrossEntropyLoss()
 
-    # pre-processing
-    A_tilde = adj + torch.eye(N)
-    D_tilde_inv_sqrt = torch.diag(torch.sqrt(torch.sum(A_tilde, dim = 1)) ** -1)
-    A_hat = torch.mm(D_tilde_inv_sqrt, torch.mm(A_tilde, D_tilde_inv_sqrt)).to(device)
-    
+    train_losses, train_accs, val_losses, val_accs = [], [], [], []
+
     # Training
     for epoch in range(args.epochs):
         t = time.time()
         network.train()
 
-        preds = network(features, A_hat) # [N, F]
+        preds = network(features, adj) # [N, F]
         train_loss = criterion(preds[idx_train], labels[idx_train])
         train_acc = accuracy(preds[idx_train], labels[idx_train])
 
@@ -66,7 +75,7 @@ def main(args):
         # validation
         with torch.no_grad():
             network.eval()
-            preds_val = network(features, A_hat)
+            preds_val = network(features, adj)
             val_loss = criterion(preds_val[idx_val], labels[idx_val])
             val_acc = accuracy(preds_val[idx_val], labels[idx_val])
 
@@ -81,12 +90,13 @@ def main(args):
         if (epoch + 1) % args.save_every == 0:
             save_checkpoint(network, 'checkpoints/epoch_%d.pt'%(epoch+1))
 
-        print('Epoch: {:04d}'.format(epoch+1),
-        'loss_train: {:.4f}'.format(train_loss.item()),
-        'acc_train: {:.4f}'.format(train_acc.item()),
-        'loss_val: {:.4f}'.format(val_loss.item()),
-        'acc_val: {:.4f}'.format(val_acc.item()),
-        'time: {:.4f}s'.format(time.time() - t))
+        train_losses.append(train_loss.item())
+        train_accs.append(train_acc.item())
+        val_losses.append(val_loss.item())
+        val_accs.append(val_acc.item())
+
+        print('[%d/%d] train loss : %.4f | train acc %.2f%% | val loss %.4f | val acc %.2f%% | time %.3fs'
+                    %(epoch+1, args.epochs, train_loss.item(), train_acc.item() * 100, val_loss.item(), val_acc.item() * 100, time.time() - t))
 
         if epochs_since_improvement > args.patience - 1 :
             print("There's no improvements during %d epochs and so stop the training."%(args.patience))
@@ -95,9 +105,32 @@ def main(args):
     # Testing
     with torch.no_grad():
         network.eval()
-        preds = network(features, A_hat)
+        preds = network(features, adj)
         test_acc = accuracy(preds[idx_test], labels[idx_test])
-        print('Test Accuracy : %.4f'%(test_acc))
+        print('Test Accuracy : %.2f'%(test_acc * 100))
+
+    # Plotting
+    fig, ax = plt.subplots()
+    ax.plot(train_losses, label = 'train loss')
+    ax.plot(val_losses, label = 'validation loss')
+    ax.legend()
+
+    ax.set(title="Loss Curve of " + args.model + " on " + args.dataset)
+    ax.grid()
+
+    fig.savefig("results/"+args.model+"_"+args.dataset+"_loss_curve.png")
+    plt.close()
+
+    fig, ax = plt.subplots()
+    ax.plot(train_accs, label = 'train accuracy')
+    ax.plot(val_accs, label = 'validation accuracy')
+    ax.legend()
+
+    ax.set(title="Accuracy Graph of " + args.model + " on " + args.dataset)
+    ax.grid()
+
+    fig.savefig("results/"+args.model+"_"+args.dataset+"_accuracy.png")
+    plt.close()
 
 if __name__  == "__main__":
     
@@ -111,13 +144,13 @@ if __name__  == "__main__":
     parser.add_argument('--save_every', type=int, default=500, help='Save every n epochs')
     parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
     parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-    parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
+    parser.add_argument('--hidden', type=int, default=64, help='Number of hidden units.')
     parser.add_argument('--n_heads', type=int, default=8, help='Number of head attentions.')
     parser.add_argument('--dropout', type=float, default=0.5, help='Dropout rate (1 - keep probability).')
     parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
     parser.add_argument('--patience', type=int, default=10, help='patience')
     parser.add_argument('--dataset', type=str, default='cora', choices=['cora','citeseer'], help='Dataset to train.')
-    parser.add_argument('--checkpoint', type=str, default='checkpoints/best.pt',help='Path to the checkpoint')
+    parser.add_argument('--model', type=str, default='GCN', choices=['GCN','GAT'], help='Model to train.')
 
     args = parser.parse_args()
 
